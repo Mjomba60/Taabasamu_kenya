f83b17b82c33b083590eb5d0edbc166bb937c5fb
Mjomba60 <geosam2708@gmail.com>
2026-01-03 20:03:12 +0300

    Added route for news and evets

---
+++ app/routes/newsnevents.tsx
@@ -0,0 +1,24 @@
+import { EventSectionContainer, NewsHero, NewsSectionContainer } from "~/components/newsneventscomponents";
+// import newsletterimg from '../images/newsletter.png'
+import '../components/routestyles/newsnevents.css'
+import { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "News and events | Taabasamu" },
+    { name: "description", content: "Our news and events" },
+  ];
+};
+
+// news are fetched from facebook developer account
+// events are fetched from ticketstripe account
+export default function NewsNEvents(){
+
+    return(
+        <div className="newsneventsmaincontainer">
+            <NewsHero/>
+            <NewsSectionContainer/>
+            <EventSectionContainer/>
+        </div>
+    )
+}
